05bafafc528621f0a4d2ea3e8d008a8f94e94fb3
Ddeepika123 <112195696+Ddeepika123@users.noreply.github.com>
2026-05-11 21:01:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
-  
+let cost = 1;
+	while(arr.length>1){
+		arr.sort((a,b) => (a-b);
+		let first = arr.shift();
+		let second = arr.shift();
+
+		let sum = first+second;
+		cost += sum;
+
+		arr.push(sum);
+	}
+  return cost;
 }
 
 module.exports=mincost;
